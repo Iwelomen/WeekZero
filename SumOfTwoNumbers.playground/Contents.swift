dfa15f15c2d53b22e751d7c13d2314e6f15046e5
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-02-01 11:03:25 +0100

    add sum of two numbers solution

---
+++ SumOfTwoNumbers.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/*
+ 
+ Create a function that takes two numbers as arguments and return their sum.
+ 
+ Examples:
+ 
+ addition(3, 2) ➞ 5
+
+ addition(-3, -6) ➞ -9
+
+ addition(7, 3) ➞ 10
+
+*/
+
+func addition(_ num1: Int, _ num2: Int) -> Int {
+    return num1 + num2
+}
+
+addition(3, 2)
+addition(-3, -6)
+addition(7, 3)
